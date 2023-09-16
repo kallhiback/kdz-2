7d5198c3b98e5397b7fbb974cba941b75eecc2d5
kallhiback <130337500+kallhiback@users.noreply.github.com>
2023-09-16 14:00:13 +0600

    kdz-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [color1, setColor1] = useState("red")
+    const [color2 , setColor2] = useState("blue")
+    function swapColors () {
+        setColor1(color2)
+        setColor2(color1)
+    }
+    return (
+        <div>
+            <div style={{
+                width: "150px",
+                height: "150px",
+                backgroundColor:color1
+            }} onClick={swapColors}></div>
+            <div style={{
+                width: "150px",
+                height: "150px",
+                backgroundColor:color2}} onClick={swapColors}></div>
+        </div>
+    );
+};
 
 export default App;
